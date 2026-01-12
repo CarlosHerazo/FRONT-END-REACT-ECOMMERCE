a4dbbea6c0c35cf59f8cd96c542e592cb031799f
CarlosHerazo <Carlosherazo2004@gmail.com>
2026-01-12 01:24:24 -0500

    feat: Comentar la importación de formatPrice en el componente ProductCard

--- src/features/products/components/ProductCard/ProductCard.tsx
+++ src/features/products/components/ProductCard/ProductCard.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'; // Importa Link
 import { type ProductCardProps } from './ProductCard.types';
 import styles from './ProductCard.module.css';
 import Icon from '../../../../shared/ui/Icon';
-import { formatPrice } from '../../../../utils/utils';
+// import { formatPrice } from '../../../../utils/utils';
 
 export const ProductCard: React.FC<ProductCardProps> = ({ 
   product, 
